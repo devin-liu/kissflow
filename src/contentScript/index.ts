f2834be02f289aeb7d76610e68b0e5e721d1fd1d
devin.r.liu@gmail.com <devin.r.liu@gmail.com>
2023-04-17 15:29:34 -0700

    Got links hiding properly

--- src/contentScript/index.ts
+++ src/contentScript/index.ts
@@ -1,49 +1,28 @@
-const hideLinks = (linksToHide: string[]) => {
-  const links = document.querySelectorAll("a");
+import "arrive";
 
-  links.forEach((link) => {
-    const href = link.getAttribute("href") || "";
+const hideLinks = (linksToHide: string[]) => {
+  document.arrive("a", { existing: true }, function () {
+    // 'this' refers to the newly created element
+    const anchorElem = this as HTMLAnchorElement;
+    const href = anchorElem.href || "";
     if (linksToHide.includes(href)) {
-      link.style.display = "none";
+      anchorElem.style.display = "none";
     }
   });
 };
 
-// const trackLinks = () => {
-//   document.querySelectorAll("a").forEach((link) => {
-//     link.addEventListener("click", async (e) => {
-//       const currentUrl = window.location.href;
-//       const startTime = Date.now();
-//       e.preventDefault();
-
-//       setTimeout(() => {
-//         const duration = Date.now() - startTime;
-//         visitDurationMap[currentUrl] = duration;
-//         saveVisitDuration();
-
-//         if (duration < 10000) {
-//           hideLinks([currentUrl]);
-//         }
-
-//         window.location.href = link.getAttribute("href") || "";
-//       }, 10);
-//     });
-//   });
-// };
-
 const init = async () => {
-  const visitDurationMap = await chrome.storage.local.get("visitDurationMap");
-  console.log("data", visitDurationMap);
+  const storageData = await chrome.storage.local.get("visitDurationMap");
+  const visitDurationMap = storageData.visitDurationMap || {};
 
-  const linksToHide = Object.keys(visitDurationMap).filter(
-    (url) => visitDurationMap[url] && visitDurationMap[url] < 10000
-  );
+  const linksToHide = Object.entries(visitDurationMap)
+    .filter(
+      ([url, duration]) => typeof duration === "number" && duration < 10000
+    )
+    .map((info) => info[0]);
   console.log("linksToHide", linksToHide);
   hideLinks(linksToHide);
-  //   trackLinks();
 };
-
-// console.log("init1");
 
 init();
 
