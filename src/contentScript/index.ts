adcfcd564b949e9ecb056ffddd753614feb45532
devin.r.liu@gmail.com <devin.r.liu@gmail.com>
2023-04-17 16:44:44 -0700

    Simplified redraw

--- src/contentScript/index.ts
+++ src/contentScript/index.ts
@@ -4,39 +4,43 @@ import $ from "cash-dom";
 const initialDisplayValues: Record<string, string> = {};
 
 const hideLinks = (linksToHide: string[]) => {
-  // hide links on load
-  document.arrive("a", { existing: true }, function () {
-    // 'this' refers to the newly created element
-    const anchorElem = this as HTMLAnchorElement;
-    const href = anchorElem.href || "";
-    if (linksToHide.includes(href)) {
-      initialDisplayValues[href] = anchorElem.style.display;
-      $(anchorElem).toggleClass("hidden-link", true);
-      $(anchorElem).toggle(false);
-      $(anchorElem).on("change", () => {
-        const newAnchorElem = this as HTMLAnchorElement;
-        console.log("change", anchorElem.href, newAnchorElem.href);
-      });
-    }
-  });
 
-  // show links when the href changes
+  const watchForChangedLink = (anchorElem: HTMLAnchorElement) => {
+    const href = anchorElem.href || "";
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        if (mutation.type === "attributes") {
+          const newAnchorElem = mutation.target as HTMLAnchorElement;
+          const newHref = newAnchorElem.href || "";
+          if (!linksToHide.includes(newHref)) {
+            console.log("change", href, newHref);
+            $(anchorElem).toggleClass("hidden-link", false);
+            $(anchorElem).toggle(true);
+            observer.disconnect();
+          }
+        }
+      });
+    });
+  
+    observer.observe(anchorElem, {
+      attributes: true,
+      childList: false,
+      characterData: false,
+    });
+  };
+  // hide links on load
   document.arrive(
     "a",
     { existing: true, fireOnAttributesModification: true },
     function () {
+      // 'this' refers to the newly created element
       const anchorElem = this as HTMLAnchorElement;
       const href = anchorElem.href || "";
-      // Check if href is NOT in the list of links to hide
-      // And if it has the hidden-link class
-      if (
-        !linksToHide.includes(href) &&
-        $(anchorElem).hasClass("hidden-link")
-      ) {
-        // Show the link
-        $(anchorElem).toggleClass("hidden-link", false);
-        $(anchorElem).toggle(true);
-        $(anchorElem).off("change");
+      if (linksToHide.includes(href)) {
+        initialDisplayValues[href] = anchorElem.style.display;
+        $(anchorElem).toggleClass("hidden-link", true);
+        $(anchorElem).toggle(false);
+        watchForChangedLink(anchorElem);
       }
     }
   );
