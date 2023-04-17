88a8eecd8751a64b97055bbd47ab3325a75d4964
devin.r.liu@gmail.com <devin.r.liu@gmail.com>
2023-04-17 16:07:13 -0700

    got links reshowing if necessary

--- src/contentScript/index.ts
+++ src/contentScript/index.ts
@@ -1,14 +1,45 @@
 import "arrive";
+import $ from "cash-dom";
+
+const initialDisplayValues: Record<string, string> = {};
 
 const hideLinks = (linksToHide: string[]) => {
+  // hide links on load
   document.arrive("a", { existing: true }, function () {
     // 'this' refers to the newly created element
     const anchorElem = this as HTMLAnchorElement;
     const href = anchorElem.href || "";
     if (linksToHide.includes(href)) {
-      anchorElem.style.display = "none";
+      initialDisplayValues[href] = anchorElem.style.display;
+      $(anchorElem).toggleClass("hidden-link", true);
+      $(anchorElem).toggle(false);
+      $(anchorElem).on("change", () => {
+        const newAnchorElem = this as HTMLAnchorElement;
+        console.log("change", anchorElem.href, newAnchorElem.href);
+      });
     }
   });
+
+  // show links when the href changes
+  document.arrive(
+    "a",
+    { existing: true, fireOnAttributesModification: true },
+    function () {
+      const anchorElem = this as HTMLAnchorElement;
+      const href = anchorElem.href || "";
+      // Check if href is NOT in the list of links to hide
+      // And if it has the hidden-link class
+      if (
+        !linksToHide.includes(href) &&
+        $(anchorElem).hasClass("hidden-link")
+      ) {
+        // Show the link
+        $(anchorElem).toggleClass("hidden-link", false);
+        $(anchorElem).toggle(true);
+        $(anchorElem).off("change");
+      }
+    }
+  );
 };
 
 const init = async () => {
